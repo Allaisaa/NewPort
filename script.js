628584d698807d5186d751041a7082a1233725e7
Allaisaa <alyssaprincess658@gmail.com>
2025-05-07 08:35:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-window.addEventListener("scroll", function(){
-    scrollBtn.classList.toggle("active", window.scrollY > 500);
-    });
-    
-        const menuBtn = document.querySelector(".menu-btn");
-        const navigation = document.querySelector(".navigation");
-        const navigationItems = document.querySelector(".navigation a");
-    
-        menuBtn.addEventListener("click", () => {
-            menuBtn.classList.toggle("active");
-            navigation.classList.toggle("active");
-        })
-    
-        // Navigation Effects
-        window.addEventListener("scroll", function(){
-            const header = document.querySelector("header");
-            header.classList.toggle("sticky", window.scrollY > 0);
-        })
-    
-        navigationItems.forEach((navigationItem) => {
-            navigationItem.addEventListener("click", () => {
-                menuBtn.classList.remove("active");
-                navigation.classList.remove("active")
-            })
-        })
-    
+function onSubmit(e) {
+    const inputEmail = document.getElementById('email');
+    const formInfo = document.getElementById('form-info');
+    const email = inputEmail.value;
+  
+    // Check if email value is lowercase or not
+    if (email !== email.toLowerCase()) {
+      e.preventDefault();
+      inputEmail.classList.add('invalid');
+      formInfo.classList.add('error');
+      formInfo.innerText = 'Error form is not sent! The Email should be in lower case!!';
+    } else {
+      inputEmail.classList.remove('invalid');
+      formInfo.classList.remove('error');
+    }
+  }
+  const contactForm = document.getElementById('contact-form');
+  contactForm.addEventListener('submit', onSubmit);
+  
+  // Remove Error onchange from the form.
+  const inputEmail = document.getElementById('email');
+  const formInfo = document.getElementById('form-info');
+  inputEmail.addEventListener('change', () => {
+    inputEmail.classList.remove('invalid');
+    formInfo.classList.remove('error');
+    formInfo.innerText = '';
+  }
+  );
